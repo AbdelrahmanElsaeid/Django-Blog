c409a0f14e072f6198ed34d8d2769a59b9d9cef0
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-01-06 13:23:10 +0200

    blog

--- djangoBlog/myblog/posts/models.py
+++ djangoBlog/myblog/posts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name="post_author")
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
+    tags = TaggableManager()
