44b732dda7c48086444edb650e87372edaec35c6
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2023-01-12 11:50:46 +0200

    customization_admin

--- djangoBlog/myblog/posts/admin.py
+++ djangoBlog/myblog/posts/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author','title']
+    list_filter = ['tags']
+    search_fields = ['title','content']
 
 
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
+
+
 
 
